4c42944e552e343462338cd044437e3126100b17
Paul Aziz <paziz75@gmail.com>
2020-08-15 16:57:57 -0700

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // helps us connect to mongo dbDatabase
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); // middleware allows us to parse json
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB databasae connection established succesfully");
 });
 
+const excercisesRouter = require("./routes/excercises");
+const usersRouter = require("./routes/users");
+
+app.use("/excerises", excercisesRouter);
+app.use("./users", usersRouter);
+
 app.listen(port, () => {
-  console.log(`ßServer is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
