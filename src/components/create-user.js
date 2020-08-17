310c63a8103e494f3ea00607033684ec24bc48e9
Paul Aziz <paziz75@gmail.com>
2020-08-16 18:30:02 -0700

    Created outline of components

---
+++ src/components/create-user.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CreateUser extends Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        <p>Create User</p>
+      </div>
+    );
+  }
+}
+
+export default CreateUser;
---
+++ src/components/exercises-list.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p> Excerise List</p>
+      </div>
+    );
+  }
+}
+
+export default ExercisesList;
